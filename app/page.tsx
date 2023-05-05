98d74e7edc4acdf2c1878e342348a35473c05444
Mateusz Kieblesz <mattkieblesz@hotmail.com>
2023-05-06 00:22:28 +0200

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,21 @@ import type { NextPage } from 'next';
 import { useState } from 'react';
 import InputForm from '../components/InputForm';
 import GoogleHeatMap from '../components/GoogleHeatMap';
-import HeatMap from '../components/HeatMap';
 
 const Home: NextPage = () => {
-  const [coordinates, setCoordinates] = useState<[number, number][]>([]);
+  const [coordinates, setCoordinates] = useState<[number, number][]>([
+    [52.237049, 21.017532],
+    [52.237289, 21.019674],
+    [52.235429, 21.015162],
+    [52.239857, 21.016392],
+    [52.238552, 21.013597],
+  ]);
 
   return (
-    <div className="h-screen">
-      <GoogleHeatMap />
-      {/* <HeatMap coordinates={coordinates} /> */}
+    <div className="h-screen flex flex-col">
+      <div className="flex-grow">
+        <GoogleHeatMap coordinates={coordinates} />
+      </div>
       <InputForm setCoordinates={setCoordinates} />
     </div>
   );
--- components/GoogleHeatMap.tsx
+++ components/GoogleHeatMap.tsx
@@ -1,51 +1,68 @@
-'use client';
-
-import React from 'react'
-
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+"use client";
+import { darkTheme } from "./dark-theme";
+import React from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  HeatmapLayer,
+} from "@react-google-maps/api";
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: "100%",
+  height: "calc(100% - 3rem)",
 };
 
 const center = {
   lat: 52.237049,
-  lng: 21.017532
+  lng: 21.017532,
 };
 
-function GoogleHeatMap() {
+type Props = {
+  coordinates: [number, number][];
+};
+
+function GoogleHeatMap({ coordinates }: Props) {
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "__REPLACE_ME__"
-  })
+    id: "google-map-script",
+    googleMapsApiKey: "__REPLACE_ME",
+    libraries: ["visualization"], // Add the 'visualization' library
+  });
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null);
 
-  const onLoad = React.useCallback(function callback(map:any) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+  const onLoad = React.useCallback(function callback(map: any) {
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
 
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
-  const onUnmount = React.useCallback(function callback(map:any) {
-    setMap(null)
-  }, [])
+  const onUnmount = React.useCallback(function callback(map: any) {
+    setMap(null);
+  }, []);
+
+  const mapOptions = {
+    styles: darkTheme,
+  };
 
   return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      options={mapOptions}
+    >
+      <HeatmapLayer
+        data={coordinates.map(
+          ([lat, lng]) => new window.google.maps.LatLng(lat, lng)
+        )}
+      />
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
-export default React.memo(GoogleHeatMap)
+export default React.memo(GoogleHeatMap);
